c15f54fa1c7e80cb1efaa732340e540cf9ab96f1
Miroslav Rutesic <miroslav.rutesic@bild-studio.net>
2020-02-14 11:21:10 +0100

    Selection and loading items in sh list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,7 +3,9 @@ import { Ingredient } from '../shared/ingredient.model'
 import { Subject } from 'rxjs';
 
 export class ShoppingListService {
-  ingredientsChanged = new Subject<Ingredient[]>();  
+  ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
+
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -11,6 +13,10 @@ export class ShoppingListService {
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
   }
 
   addIngredient(ingredient: Ingredient) {
