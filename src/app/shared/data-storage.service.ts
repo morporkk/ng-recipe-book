bf5a9edc45e745349f60e7e713a642ec24a33bea
Miroslav Rutesic <miroslav.rutesic@bild-studio.net>
2020-02-19 11:26:48 +0100

    Attach inteceptor to token

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -22,27 +22,20 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.http.get<Recipe[]>(
-          'https://ng-recipe-book-1470f.firebaseio.com/recipes.json',
-          {
-            params: new HttpParams().set('auth', user.token)
-          }
-        );
-      }),
-      map(recipes => {
-        return recipes.map(recipe => {
-          return {
-            ...recipe,
-            ingredients: recipe.ingredients ? recipe.ingredients : []
+    return this.http.get<Recipe[]>(
+      'https://ng-recipe-book-1470f.firebaseio.com/recipes.json'
+    ).pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
           };
-        });
-      }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes);
-      })
-    );
+      });
+    }),
+    tap(recipes => {
+      this.recipeService.setRecipes(recipes);
+    })
+  );
   }
 }
